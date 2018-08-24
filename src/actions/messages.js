1b4dde2092a3bc0b9b7d2184a468e7a99ed7620e
tgentry300 <taylor.gentry300@gmail.com>
2018-08-24 15:43:32 -0400

    added basic fetch actions for ten or all messages

---
+++ src/actions/messages.js
@@ -0,0 +1,59 @@
+export const GET_ALL_MESSAGES_REQUEST = 'GET_ALL_MESSAGES_REQUEST'
+export const GET_ALL_MESSAGES_RESPONSE = 'GET_ALL_MESSAGES_RESPONSE'
+export const GET_TEN_MESSAGES_REQUEST = 'GET_ALL_MESSAGES_REQUEST'
+export const GET_TEN_MESSAGES_RESPONSE = 'GET_ALL_MESSAGES_RESPONSE'
+
+const MESSAGE_API_URL = 'https://kwitter-api.herokuapp.com/messages'
+
+export function fetchTenMessagesAsync() {
+    return dispatch => {
+        dispatch(getTenMessagesRequest())
+        
+        fetch(`${MESSAGE_API_URL}?limit=10`)
+            .then(res => res.json())
+            .then(data => {
+                dispatch(getTenMessagesResponse(data))
+                console.log(data)
+            })
+    }
+    
+}
+
+export function fetchAllMessagesAsync() {
+    return dispatch => {
+        dispatch(getAllMessagesRequest())
+        
+        fetch(MESSAGE_API_URL)
+            .then(res => res.json())
+            .then(data => {
+                dispatch(getAllMessagesResponse(data))
+            })
+    }
+    
+}
+
+
+const getTenMessagesRequest = () => {
+    return {
+        type: GET_TEN_MESSAGES_REQUEST
+    }
+}
+
+const getTenMessagesResponse = (data) => {
+    return {
+        type: GET_TEN_MESSAGES_RESPONSE
+
+    }
+}
+
+const getAllMessagesRequest = () => {
+    return {
+        type: GET_ALL_MESSAGES_REQUEST
+    }
+}
+
+const getAllMessagesResponse = (data) => {
+    return {
+        type: GET_ALL_MESSAGES_RESPONSE
+    }
+}
