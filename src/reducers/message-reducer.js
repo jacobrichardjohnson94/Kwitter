4ae6bd0c5a454bc934de56aa00aff86e1dbff8dc
tgentry300 <taylor.gentry300@gmail.com>
2018-08-26 15:39:38 -0400

    message actions and message reducers are completed

--- src/reducers/message-reducer.js
+++ src/reducers/message-reducer.js
@@ -4,11 +4,13 @@ import {GET_ALL_MESSAGES_REQUEST,
         GET_TEN_MESSAGES_RESPONSE } 
         from './actions/messages.js';
 
-const initialGetAllMessagesState = {
+const initialMessagesState = {
   fetching: false,
   messages: [],
 };
-export function getAllMessagesReducer(state = initialGetAllMessagesState, action) {
+
+
+export function getMessagesReducer(state = initialMessagesState, action) {
   switch (action.type) {
     case GET_ALL_MESSAGES_REQUEST:
       return {
@@ -20,6 +22,18 @@ export function getAllMessagesReducer(state = initialGetAllMessagesState, action
         messages: action.messages,
         fetching: false,
       };
+    case GET_TEN_MESSAGES_REQUEST:
+      return {
+        ...state,
+        fetching: true,
+      };
+    case GET_TEN_MESSAGES_RESPONSE:
+      return {
+        messages: action.messages,
+        fetching: false
+      };
   }
 }
+
+
 
