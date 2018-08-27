6537b2443ea4c4799c150a7d32fc17a3c1d70cc6
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-08-27 16:17:51 -0400

    async working

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,34 +1,27 @@
-import React, { Component } from "react";
-import { List, Image } from "semantic-ui-react";
-import SingleMessage from "./SingleMessage";
-import {
-  fetchTenMessagesAsync,
-  fetchAllMessagesAsync
-} from "../actions/messages";
-import { fetchAllUsersAsync } from '../actions/fetch-all-users.js'
-import { connectRouter } from "connected-react-router";
-import {connect} from "react-redux"
+import React, { Component } from 'react';
+import { List, Image } from 'semantic-ui-react';
+import SingleMessage from './SingleMessage';
+import { fetchTenMessagesAsync, fetchAllMessagesAsync } from '../actions/messages';
+import { fetchAllUsersAsync } from '../actions/fetch-all-users.js';
+import { connectRouter } from 'connected-react-router';
+import { connect } from 'react-redux';
 import koalaIcon from '../resources/images/koalaIcon.svg';
 import { Dimmer, Loader } from 'semantic-ui-react';
-
-
+import { withRouter } from 'react-router-dom';
 
 const style = {
   // list: {
   //   marginTop: "5em",
   // },
   container: {
-    marginTop:'1em',
-    height: "23em",
+    marginTop: '1em',
+    height: '23em',
     overflowX: 'hidden',
-    overflowY: 'scroll'
-
-  }
+    overflowY: 'scroll',
+  },
 };
 
 class MessageList extends Component {
-
-
   // componentDidMount(){
   // fetch('https://kwitter-api.herokuapp.com/users')
   // .then(res=>res.json())
@@ -36,19 +29,11 @@ class MessageList extends Component {
   //   this.setState({userList: data})
   //   // console.log(this.state)
   // })
-  
-  
-  
-  componentWillMount() {
-      this.props.fetchAllUsers()
-    
-      if (this.props.loggedIn) {
-        this.props.fetchAllMessagesList();
-      } else {
-        this.props.fetchTenMessagesList();
-      }
-    }
-    
+
+  componentDidMount() {
+    this.props.fetchAllUsers();
+  }
+
   render() {
     return (
       <div style={style.container}>
@@ -59,23 +44,24 @@ class MessageList extends Component {
         ) : null}
         <List style={style.list} divided verticalAlign="middle">
           {this.props.messageList.map(message => {
-            let displayName
-            let username
+            let displayName;
+            let username;
             this.props.userList.users.forEach(user => {
-              if(message.userId===user.id){
-                displayName = user.displayName
-                username = user.userName
+              if (message.userId === user.id) {
+                displayName = user.displayName;
+                username = user.userName;
               }
-            })
-            return <SingleMessage
-            key={message.id}
-            message={message.text}
-            createdDate={message.createdAt}
-            displayName={displayName}
-            username={username}
-            />
-          }
-          )}
+            });
+            return (
+              <SingleMessage
+                key={message.id}
+                message={message.text}
+                createdDate={message.createdAt}
+                displayName={displayName}
+                username={username}
+              />
+            );
+          })}
         </List>
       </div>
     );
@@ -87,7 +73,7 @@ const mapStateToProps = state => {
     loggedIn: state.loginUser.loggedIn,
     messageList: state.getMessages.messages,
     userList: state.getAllUsers.userList,
-    fetching: state.getAllUsers.fetching
+    fetching: state.getAllUsers.fetching,
   };
 };
 
@@ -95,8 +81,13 @@ const mapDispatchToProps = dispatch => {
   return {
     fetchTenMessagesList: () => dispatch(fetchTenMessagesAsync()),
     fetchAllMessagesList: () => dispatch(fetchAllMessagesAsync()),
-    fetchAllUsers: () => dispatch(fetchAllUsersAsync())
+    fetchAllUsers: () => dispatch(fetchAllUsersAsync()),
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(MessageList)
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(MessageList)
+);
