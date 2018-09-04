1e243e6a0195daf635a89d2b3842a757ea200886
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-09-04 14:22:44 -0400

    working

--- src/store.js
+++ src/store.js
@@ -4,14 +4,29 @@ import { connectRouter, routerMiddleware } from 'connected-react-router';
 import { createUserReducer, loginUserReducer } from './reducers/user-reducer.js';
 import thunk from 'redux-thunk';
 import { getMessagesReducer } from './reducers/message-reducer.js';
-import { getAllUsersReducer } from './reducers/fetch-all-users-reducer.js'
+import { getAllUsersReducer } from './reducers/fetch-all-users-reducer.js';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const rootReducer = combineReducers({ createUser: createUserReducer, loginUser: loginUserReducer, getMessages: getMessagesReducer, getAllUsers: getAllUsersReducer});
+const rootReducer = combineReducers({
+  createUser: createUserReducer,
+  loginUser: loginUserReducer,
+  getMessages: getMessagesReducer,
+  getAllUsers: getAllUsersReducer,
+});
 
 export const history = createBrowserHistory();
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = createStore(
-  connectRouter(history)(rootReducer),
+  connectRouter(history)(persistedReducer),
   composeEnhancers(applyMiddleware(routerMiddleware(history), thunk))
 );
+export let persistor = persistStore(store);
